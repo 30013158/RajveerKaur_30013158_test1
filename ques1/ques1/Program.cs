dfb262373a292ec56f78200a296a72c0cd464812
Rajveer Kaur <30013158@student.toiohomai.ac.nz>
2019-04-10 11:29:08 +1200

    first commit

---
+++ ques1/ques1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Name: Rajveer Kaur
+task 1 = ques1*/ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ques1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine("-------------This program can give the result of base^exponent----------------");
+            Console.WriteLine("------------------------------------------------------------------------------");
+
+            //obtaining base value in double variable
+            Console.WriteLine("Enter the value of the base: ");
+            double bottom = double.Parse(Console.ReadLine());
+
+            //obtaining exponent value in integer variable
+            Console.WriteLine("Enter the value of the exponent: ");
+            int expo = int.Parse(Console.ReadLine());
+
+            
+            //displaying result and message           
+            Console.WriteLine($"{bottom}^{expo} = {bottom*expo}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ques2/ques2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*NAME:RAJVEER KAUR
+ TASK :ques 2 */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ques2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("-----This program will give the sum total of 5 prices-------");
+            Console.WriteLine("------------------------------------------------------------");
+            double total = 0;//initialising double variable
+            for (int i = 1; i <= 5; i++)
+            { Console.WriteLine($"Enter the price of item {i}: ");
+                total += double.Parse(Console.ReadLine());
+                //displaying the total of all items
+            }
+            //representing the message and total
+            Console.WriteLine($"The total of those items is ${total}");
+            Console.ReadLine();
+        }
+    }
+}
